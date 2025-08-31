35478b4506dbf5df1eaa47d9b5616dcbbe88b5e2
vikas <vikaskumar783588@gmail.com>
2025-08-31 19:49:48 +0530

    completed test

--- programs/counter-pda/src/lib.rs
+++ programs/counter-pda/src/lib.rs
@@ -6,11 +6,55 @@ declare_id!("JCmHRfx897eap7kkkDiQUHj9aNu6HJmVFqKZANMMQzwb");
 pub mod counter_pda {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_counter(ctx: Context<CreateCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        counter.authority = ctx.accounts.authority.key();
+
+        msg!("creating a counter");
+
+        msg!("current counter is {}", counter.count);
+        msg!("the admin pubkey is this {}", counter.authority);
+        Ok(())
+    }
+
+    pub fn update_counter(ctx: Context<UpdateCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        msg!("adding one to the counter");
+
+        counter.count += 1;
+
+        msg!("Current counter is {}", counter.count);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateCounter<'info> {
+    #[account(mut)]
+    authority: Signer<'info>,
+    #[account(
+        init,
+        seeds = [b"counter1", authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 8 + 32 + 8
+    )]
+    counter: Account<'info, Counter>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateCounter<'info> {
+    authority: Signer<'info>,
+    #[account(mut, has_one = authority)]
+    counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    authority: Pubkey,
+    count: u64,
+}
--- tests/counter-pda.ts
+++ tests/counter-pda.ts
@@ -1,16 +1,52 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CounterPda } from "../target/types/counter_pda";
+import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
 
 describe("counter-pda", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.counterPda as Program<CounterPda>;
+  const provider = anchor.AnchorProvider.local();
 
-  it("Is initialized!", async () => {
+  it("counter initialized!", async () => {
+    let [counter] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("counter1"), provider.publicKey.toBytes()],
+      program.programId
+    );
+    console.log("your program address is :", counter.toString());
+
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    // const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .createCounter()
+      .accounts({
+        authority: provider.publicKey,
+        counter: counter,
+        systemProgram: SYSTEM_PROGRAM_ID,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
+  });
+
+  it("Counter updated", async () => {
+    let [counter_pubkey] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("counter1"), provider.publicKey.toBytes()],
+      program.programId
+    );
+    const counter = await program.account.counter.fetch(counter_pubkey);
+    console.log("current counter is: ", counter.count.toNumber());
+    const tx = await program.methods
+      .updateCounter()
+      .accounts({
+        authority: provider.publicKey,
+        counter: counter_pubkey,
+      })
+      .rpc();
+
+    console.log("txn", tx);
+    const updated_counter = await program.account.counter.fetch(counter_pubkey);
+    console.log("now the counter is:", updated_counter.count.toNumber());
   });
 });
